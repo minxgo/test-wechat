b3dc2bf4b1d20d9549d7ed4900cb0b963cc29471
minxgo <minxgo@163.com>
2018-12-28 17:27:55 +0800

    update source

--- pages/movies/more-movies.js
+++ pages/movies/more-movies.js
@@ -4,7 +4,30 @@ const util = require("../../utils/util.js");
 Page({
 
   data: {
-    movies: {}
+    movies: {},
+    totalCount: 0,
+    requestUrl: "",
+    isEmpty: true
+  },
+
+  // 下拉刷新
+  onPullDownRefresh() {
+    var refreshUrl = this.data.requestUrl + "?start=0&count=20";
+    this.setData({ movies: {}, isEmpty: true });
+    util.http(refreshUrl, this.callBack); //获取数据
+    wx.showNavigationBarLoading();
+  },
+
+  // 上拉刷新
+  onScrollUpper() {
+    this.onPullDownRefresh();
+  },
+
+  // 触底刷新
+  onScrollLower() {
+    var nextUrl = this.data.requestUrl + "?start=" + this.data.totalCount + "&count=20";
+    util.http(nextUrl, this.callBack); //获取数据
+    wx.showNavigationBarLoading();
   },
 
   onLoad(options) {
@@ -22,11 +45,12 @@ Page({
         dataUrl = app.globalData.doubanBase + "/v2/movie/top250";
         break;
     }
-    util.http(dataUrl, this.callBack)
+    this.setData({ requestUrl: dataUrl });
+    util.http(dataUrl, this.callBack);
   },
 
   callBack(data) {
-    var movies = [], data = data.subjects;
+    var movies = [], data = data.subjects, totalMovies = [];
     for (var e in data) {
       var subject = data[e], title = subject.title;
       if (title.length >= 6) {
@@ -41,8 +65,15 @@ Page({
       }
       movies.push(temp);
     }
-    this.setData({ movies });
-    console.log(this.data.movies)
+    if ( !this.data.isEmpty ) {
+      totalMovies = this.data.movies.concat(movies);
+    } else {
+      totalMovies = movies;
+      this.setData({ isEmpty: false });
+    }
+    var totalCount = this.data.totalCount + 20;
+    this.setData({ movies: totalMovies, totalCount });
+    wx.hideNavigationBarLoading();
   },
 
   onReady() {
